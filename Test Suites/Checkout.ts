59a27aa5857f27843ed2e55033388b33306eef94
095047 <095047@LAPTOP-BRAVO19.bfi.co.id>
2022-07-20 15:19:48 +0700

    tes katalon git

--- Test Suites/Checkout.ts
+++ Test Suites/Checkout.ts
@@ -12,6 +12,25 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>afaa3054-a00e-4134-844b-2d3f5cf0c585</testSuiteGuid>
    <testCaseLink>
+      <guid>77b8add2-185e-4373-8f97-5bea205fe0cc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Verify cart items</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>4f01bf6d-5fbc-41fb-bc67-8de91b924c22</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>cbbbff5b-3e81-4077-9306-b80a6819a481</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
       <guid>1e22fe71-fe9e-4e21-959f-8205c61b2968</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
